119d9912e2b35ea2dee1ac2a5dc0d520216c1ba3
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-04-28 22:34:16 -0300

    spring initializr

---
+++ api-notivis/src/main/java/com/npteam/apinotivis/ApiNotivisApplication.java
@@ -0,0 +1,13 @@
+package com.npteam.apinotivis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotivisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiNotivisApplication.class, args);
+  }
+
+}
---
+++ api-notivis/src/test/java/com/npteam/apinotivis/ApiNotivisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npteam.apinotivis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotivisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
